91465101255bb6613592c2d003c17051d83678c3
Greg Dodd <gdodd97@gmail.com>
2024-10-15 11:21:55 -0400

    Added next cell and next state functions. Added testing script and it is fully passing.

--- gameoflife.py
+++ gameoflife.py
@@ -1,4 +1,5 @@
 import random
+import time
 board_state = []
 
 def dead_state(dead_width, dead_height):
@@ -19,20 +20,82 @@ def random_state(board_width, board_height):
                 state[i][j] = 1
     return state
 
+def state_width(state):
+    return len(state)
+
+def state_height(state):
+    return len(state[0])
+
 def render(state):
-    for i in state:
-        print('|', end = ' ')
-        for j in i:
-            if j == 0:
-                print('X', end =  ' ')
-            if j == 1:
-                print('O', end = ' ')
-        print('|\n')
 
-def next_board_state(state):
-    #todo create function to intake current board state and calculate next board state. 
+    display_as = {
+        0: ' ',
+        # This is "unicode" for a filled-in square. You can also just use a thick
+        # "ASCII" character like a '$' or '#'.
+        1: u"\u2588"
+    }
+    lines = []
+    for y in range(0, state_height(state)):
+        line = ''
+        for x in range(0, state_width(state)):
+            line += display_as[state[x][y]] * 2
+        lines.append(line)
+    print ("\n".join(lines))
 
-    return state
+def next_cell_value(cell_coords,state):
+    width = state_width(state)
+    height = state_height(state)
+    x = cell_coords[0]
+    y = cell_coords[1]
+    n_live_neighbors = 0
 
-a_random_state = random_state(3,3)
-render(a_random_state)
+    # Iterate around this cell's neighbors
+    for x1 in range((x-1), (x+1)+1):
+        # Make sure we don't go off the edge of the board
+        if x1 < 0 or x1 >= width: continue
+
+        for y1 in range((y-1), (y+1)+1):
+            # Make sure we don't go off the edge of the board
+            if y1 < 0 or y1 >= height: continue
+            # Make sure we don't count the cell as a neighbor of itself!
+            if x1 == x and y1 == y: continue
+
+            if state[x1][y1] == 1:
+                n_live_neighbors += 1
+
+    if state[x][y] == 1:
+        if n_live_neighbors <= 1:
+            return 0
+        elif n_live_neighbors <= 3:
+            return 1
+        else:
+            return 0
+    else:
+        if n_live_neighbors == 3:
+            return 1
+        else:
+            return 0
+
+def next_board_state(init_state):
+
+    width = state_width(init_state)
+    height = state_height(init_state)
+    next_state = dead_state(width, height)
+
+    for x in range(0, width):
+        for y in range(0, height):
+            next_state[x][y] = next_cell_value((x, y), init_state)
+
+    return next_state
+
+def run_forever(init_state):
+    next_state = init_state
+    while True:
+        render(next_state)
+        next_state = next_board_state(next_state)
+        time.sleep(0.03)
+
+if __name__ == "__main__":
+    init_state = random_state(5, 5)
+    # init_state = load_board_state('./toad.txt')
+    run_forever(init_state)
